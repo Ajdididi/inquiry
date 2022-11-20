3ce8cefb885f4ea217fe81a3c8cb89d296915726
Ajdididi <anjokosei.druginfo@gmail.com>
2022-11-20 15:09:25 +0900

    Auth

--- app.py
+++ app.py
@@ -70,29 +70,28 @@ def serchInNew():  #DBファイルの余分な行・列を削除する
         df_result = df_result.sort_index()
     return df_result
 
-#if check_password():  # JSONファイルにpasswordを設定し、ダッシュボードのsecret設定へ（元ファイルはignor設定）
+if check_password():  # JSONファイルにpasswordを設定し、ダッシュボードのsecret設定へ（元ファイルはignor設定）
+    st.title('問合せ記録 横断検索')
+    kensaku = st.text_input('検索ワード（1単語のみ）を入力してください。  ※半角・全角は区別されます')
+    btn = st.button('検索')
+    if btn:
+        st.write('===== データベース使用上の注意 =====')
+        st.write('問合せ記録は、過去の対応事例の内容を示すデータベースです。')
+        st.write('対応時点で根拠とした医薬品情報が現在も同じエビデンスレベルで活用できるとは限らないことにご留意ください。')
+        st.write('※このページからは情報の修正はできません。修正が必要な場合は管理担当者へ連絡してください。')
 
-st.title('問合せ記録 横断検索')
-kensaku = st.text_input('検索ワード（1単語のみ）を入力してください。  ※半角・全角は区別されます')
-btn = st.button('検索')
-if btn:
-    st.write('===== データベース使用上の注意 =====')
-    st.write('問合せ記録は、過去の対応事例の内容を示すデータベースです。')
-    st.write('対応時点で根拠とした医薬品情報が現在も同じエビデンスレベルで活用できるとは限らないことにご留意ください。')
-    st.write('※このページからは情報の修正はできません。修正が必要な場合は管理担当者へ連絡してください。')
+        df_result = serchInNew()
+        st.write(f'■2022年1月以降の問合せ記録:  {len(df_result)} 件')
+        st.dataframe(df_result, 
+                     #width=1000, 
+                     #use_container_width=False
+                     )
 
-    df_result = serchInNew()
-    st.write(f'■2022年1月以降の問合せ記録:  {len(df_result)} 件')
-    st.dataframe(df_result, 
-                 #width=1000, 
-                 #use_container_width=False
-                 )
-
-    df_result = serchInOld()
-    st.write(f'■2021年12月以前の問合せ記録:  {len(df_result)} 件')
-    st.dataframe(df_result, 
-                 #width=1000, 
-                 #use_container_width=True
-                 )
+        df_result = serchInOld()
+        st.write(f'■2021年12月以前の問合せ記録:  {len(df_result)} 件')
+        st.dataframe(df_result, 
+                     #width=1000, 
+                     #use_container_width=True
+                     )
 
     
